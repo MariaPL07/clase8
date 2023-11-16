cd6ebab54b1a5c65e254db609242a2ff0576d729
maria jose <mariajosemj2013jk@gmail.com>
2023-11-16 12:30:44 -0500

    crear varias listas y muestra su tamaño, cre el .gitignore

---
+++ main.py
@@ -0,0 +1,35 @@
+
+'''Hasta ahora hemos estado
+trabajando con variables que permiten almacenar
+un unico valor'''
+
+edad = 12
+
+altura = 1.79
+
+nombre = "Juan"
+
+estado = True
+
+'''En python podemos usar
+una variable que almacena una coleccion
+de datos y luego accederla usando un subindice'''
+
+# Lista de enteros
+lista1 = [10, 5, 3, 9]
+
+# Lista de decimales
+lista2 = [1.78, 2.66, 1.55, 89.4]
+
+# Lista de string
+lista3 = ["Lunes", "Martes", "Miercoles"]
+
+'''Lista de elementos 
+de distinto tipo'''
+
+lista4 = ["Juan",45,1.92,False]
+
+
+
+if __name__ == '__main__':
+
